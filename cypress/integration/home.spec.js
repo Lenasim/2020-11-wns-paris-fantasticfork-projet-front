c4b809b5f627884424e57025877f75b32743708b
lenasim <radical.rin@gmail.com>
2021-12-28 16:52:47 +0100

    cypress test

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -8,4 +8,38 @@ describe('Main Test', () => {
       expect(location.pathname).to.eq('/login')
     })
   })
+
+  it('Redirect to login page', () => {
+    cy.location().should((location) => {
+      expect(location.pathname).to.eq('/login')
+    })
+  })
+
+  it('sets auth token when logging in via form submission', () => {
+    cy.get('#email')
+    .should('be.visible')
+    .should('have.focus')
+    .type("test5@test.com")
+    
+    cy.get('#password')
+    .should('be.visible')
+    .type("testtest")
+
+    cy.get('.MuiButton-label')
+      .should('be.visible')
+      .click()
+
+    cy.location().should((location) => {
+      expect(location.pathname).to.eq('/home')
+    })
+
+    cy.should(() => {
+      expect(localStorage.getItem('stud-connect@firstname')).to.eq('lena')
+      expect(localStorage.getItem('stud-connect@lastname')).to.eq('test')
+      expect(localStorage.getItem('stud-connect@role')).to.eq('STUDENT')
+      expect(localStorage.getItem('stud-connect@token')).to.be.a('string')
+    })
+
+    cy.get('.flex_column > .MuiTypography-root').should('contain', 'lena test')
+  })
 })
