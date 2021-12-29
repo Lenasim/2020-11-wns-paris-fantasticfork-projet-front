c767c8a5221f1f2b7df855548f79d547ba8c0125
lenasim <radical.rin@gmail.com>
2021-12-29 17:36:27 +0100

    manage uncaught exception for tests

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -16,5 +16,10 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 
+// eslint-disable-next-line no-unused-vars
+Cypress.on('uncaught:exception', (err, runnable) => 
+  // returning false here prevents Cypress from failing the test
+  false
+)
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
